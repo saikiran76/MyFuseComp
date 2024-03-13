59d7ec02f5fa8e53fdbbaa0edbaa32ac0c3c358c
unknown <kskkorada76@gmail.com>
2024-03-13 13:25:52 +0530

    redesigned the login

--- src/components/login.js
+++ src/components/login.js
@@ -1,18 +1,31 @@
 const Login = () => {
     return(
-        <div class="bg-gradient-to-r from-purple-500 to-pink-500 p-8 shadow-lg text-white font-poppins text-center">
-            <h1 class="text-5xl font-bold text-center mb-6">Unlock Endless Opportunities</h1>
-            <div class="text-center mb-6">
-                <p class="text-xl mb-2">Discover a world of opportunities with:</p>
-                <ul class="list-none list-inside text-xl text-white">
-                <li>Internships</li>
-                <li>Projects</li>
-                <li>A gateway to your professional future</li>
-                </ul>
+        <div className="bg-blue-900 p-8 rounded-lg shadow-lg m-4">
+            <div className="flex flex-col lg:flex-row items-center">
+                <div className="lg:w-1/2">
+                <h1 className="text-5xl font-bold text-white mb-6 font-['Poppins']">Unlock Endless Opportunities</h1>
+                <p className="text-xl text-gray-300 mb-6 font-['Poppins']">Discover a world of opportunities with Internships, projects and lot more..</p>
+                <form className="w-full max-w-md">
+                    <div className="flex items-center border-b-2 border-teal-500 py-2">
+                    {/* <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="email" placeholder="Insert email address"/>
+                    <button className="flex-shrink-0 bg-lime-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded-lg font-['Poppins']" type="button" >Sign Up</button> */}
+                    </div>
+                </form>
+                <div classNameName="mt-6">
+                    <div className="flex items-center font-['Poppins']">
+                    <div className="text-xl font-bold text-yellow-400 mr-2 my-2 font-['Poppins']">Join a vibrant community that paves the way to success.</div>
+                    </div>
+                </div>
+                </div>
+                <div className="lg:w-1/2 mt-8 lg:mt-0">
+                    <img className="w-full rounded-lg" src="./images/success.png" ></img>
+                </div>
             </div>
-            <p class="text-xl text-center mb-6">Join a vibrant community that paves the way to success.</p>
-            <button class="bg-white text-purple-500 font-bold py-3 px-6 rounded-xl shadow-lg hover:bg-purple-500 hover:text-white transition-colors duration-300">Join Now</button>
+            <div className="mt-8 text-center">
+                <button className="bg-lime-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded-lg font-['Poppins']">Join us</button>
+            </div>
         </div>
+    
     )
 }
 
